82d24e232b2ab21cadc699ab1e2667cdb4927ed9
ElzaanVan <75164867+ElzaanVan@users.noreply.github.com>
2021-10-03 14:12:03 +0200

    Basic Pagination implemented

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import axios from 'axios';
 import { useEffect, useState } from 'react';
+
 import './App.css';
+import { Pagination } from './components/Pagination';
+import { Posts } from './components/Posts';
+
 
 
 function App() {
   const [posts, setPosts] = useState([]);
   const [loading, setLoading] = useState(false);
   const [currentPage, setCurrentPage] = useState(1);
-  const [postsPerPage, setPostsPerPage] = useState(10);
+  const [postsPerPage] = useState(10);
 
   useEffect(() => {
     const fetchPosts = async() => {
@@ -19,10 +23,20 @@ function App() {
     fetchPosts();
   }, []);
 
-  console.log(posts);
+  // Get current posts
+  const indexOfLastPost = currentPage * postsPerPage;
+  const indexOfFirstPost = indexOfLastPost - postsPerPage;
+  const currentPosts = posts.slice(indexOfFirstPost,indexOfLastPost);
+
+  // Paginate
+  const paginate = (pageNumber) => setCurrentPage(pageNumber);
+
+  // console.log(posts);
   return (
-    <div className="App">
-      Hello
+    <div className="App mt-5">
+      <h1>The Posts</h1>
+      <Posts posts={currentPosts} loading={loading} />
+      <Pagination postsPerPage={postsPerPage} totalPosts={posts.length} paginate={paginate}/>
     </div>
   );
 }
